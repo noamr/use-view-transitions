8c81965109b3dc80de4742eb0e8080e73648cc79
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-16 15:36:42 +0200

    Add mood picker example

---
+++ examples/react/mood-picker.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { flushSync } from 'react-dom'
+import { useViewTransition } from '../../src/react-vt'
+
+const moods = [
+	{ emoji: '🙂', label: 'Happy' },
+	{ emoji: '😂', label: 'LOL' },
+	{ emoji: '😎', label: 'Chill' },
+	{ emoji: '😢', label: 'Sad' },
+	{ emoji: '🦄', label: "I'm a unicorn" },
+]
+
+export function MooodPicker() {
+	const [currentMoodIndex, setCurrentMoodIndex] = useState(0)
+	const { startViewTransition } = useViewTransition()
+	return (
+		<div>
+			{moods.map(({ emoji }, index) => (
+				<button
+					onClick={() => {
+						// startViewTransition doesn't work 🤷‍♀️
+						// startViewTransition(() => {
+						// 	setCurrentMoodIndex(index)
+						// })
+						document.startViewTransition(() =>
+							flushSync(() => {
+								setCurrentMoodIndex(index)
+							}),
+						)
+					}}
+					disabled={index === currentMoodIndex}
+				>
+					<span
+						style={{
+							viewTransitionName:
+								index === currentMoodIndex ? undefined : `mood-${index}`,
+						}}
+					>
+						{emoji}
+					</span>
+				</button>
+			))}
+			<div>
+				<span
+					style={{
+						viewTransitionName: `mood-${currentMoodIndex}`,
+						fontSize: '5em',
+					}}
+				>
+					{moods[currentMoodIndex].emoji}
+				</span>
+				<div style={{ fontSize: '2em' }}>{moods[currentMoodIndex].label}</div>
+			</div>
+		</div>
+	)
+}
--- examples/react/react-simple.jsx
+++ examples/react/react-simple.jsx
@@ -4,13 +4,16 @@ import './App.css'
 import { SquareWithImmediateTransition } from "./immediate";
 import { SquareWithDelayedTransition} from "./delayed";
 import "./App.css";
+import { MooodPicker } from './mood-picker';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    Immediate transition:
+    <h2>Immediate transition:</h2>
     <SquareWithImmediateTransition />
-    Suspended Transition:
+    <h2>Suspended Transition:</h2>
     <SquareWithDelayedTransition />
+    <h2>Mood picker:</h2>
+    <MooodPicker />
   </React.StrictMode>,
 )
