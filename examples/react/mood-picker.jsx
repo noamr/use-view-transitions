ed5d2af2f2b2489421999ff012cf9f479ac671a6
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-19 13:18:55 +0200

    Mood picker: convert tabs to spaces for consistency

--- examples/react/mood-picker.jsx
+++ examples/react/mood-picker.jsx
@@ -2,48 +2,48 @@ import { useState } from 'react'
 import { useViewTransition } from '../../src/react-vt'
 
 const moods = [
-	{ emoji: '🙂', label: 'Happy' },
-	{ emoji: '😂', label: 'LOL' },
-	{ emoji: '😎', label: 'Chill' },
-	{ emoji: '😢', label: 'Sad' },
-	{ emoji: '🦄', label: "I'm a unicorn" },
+  { emoji: '🙂', label: 'Happy' },
+  { emoji: '😂', label: 'LOL' },
+  { emoji: '😎', label: 'Chill' },
+  { emoji: '😢', label: 'Sad' },
+  { emoji: '🦄', label: "I'm a unicorn" },
 ]
 
 export function MooodPicker() {
-	const [currentMoodIndex, setCurrentMoodIndex] = useState(0)
-	const { startViewTransition } = useViewTransition()
-	return (
-		<div>
-			{moods.map(({ emoji }, index) => (
-				<button
-					onClick={() => {
-						startViewTransition(() => {
-							setCurrentMoodIndex(index)
-						})
-					}}
-					disabled={index === currentMoodIndex}
-				>
-					<span
-						style={{
-							viewTransitionName:
-								index === currentMoodIndex ? undefined : `mood-${index}`,
-						}}
-					>
-						{emoji}
-					</span>
-				</button>
-			))}
-			<div>
-				<span
-					style={{
-						viewTransitionName: `mood-${currentMoodIndex}`,
-						fontSize: '5em',
-					}}
-				>
-					{moods[currentMoodIndex].emoji}
-				</span>
-				<div style={{ fontSize: '2em' }}>{moods[currentMoodIndex].label}</div>
-			</div>
-		</div>
-	)
+  const [currentMoodIndex, setCurrentMoodIndex] = useState(0)
+  const { startViewTransition } = useViewTransition()
+  return (
+    <div>
+      {moods.map(({ emoji }, index) => (
+        <button
+          onClick={() => {
+            startViewTransition(() => {
+              setCurrentMoodIndex(index)
+            })
+          }}
+          disabled={index === currentMoodIndex}
+        >
+          <span
+            style={{
+              viewTransitionName:
+                index === currentMoodIndex ? undefined : `mood-${index}`,
+            }}
+          >
+            {emoji}
+          </span>
+        </button>
+      ))}
+      <div>
+        <span
+          style={{
+            viewTransitionName: `mood-${currentMoodIndex}`,
+            fontSize: '5em',
+          }}
+        >
+          {moods[currentMoodIndex].emoji}
+        </span>
+        <div style={{ fontSize: '2em' }}>{moods[currentMoodIndex].label}</div>
+      </div>
+    </div>
+  )
 }
